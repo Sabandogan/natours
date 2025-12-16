3070f020e1a7a55f5c801db72a863c11f5b29126
Şaban Doğan <doogansaban@gmail.com>
2025-12-16 20:35:07 +0300

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
+
+//Start express app
 const app = express();
 
 app.set('view engine', 'pug');
